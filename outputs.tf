5e23b311aa2628b0876a8475e97cf5fd2b1c2e66
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:44:19 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "analysis_services_servers" {
+  description = "All analysis_services_server resources"
+  value       = azurerm_analysis_services_server.analysis_services_servers
+}
+
---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "analysis_services_servers" {
+  description = <<EOT
+Map of analysis_services_servers, attributes below
+Required:
+    - location
+    - name
+    - resource_group_name
+    - sku
+Optional:
+    - admin_users
+    - backup_blob_container_uri
+    - power_bi_service_enabled
+    - querypool_connection_mode
+    - tags
+    - ipv4_firewall_rule (block):
+        - name (required)
+        - range_end (required)
+        - range_start (required)
+EOT
+
+  type = map(object({
+    location                  = string
+    name                      = string
+    resource_group_name       = string
+    sku                       = string
+    admin_users               = optional(set(string))
+    backup_blob_container_uri = optional(string)
+    power_bi_service_enabled  = optional(bool)
+    querypool_connection_mode = optional(string, "All")
+    tags                      = optional(map(string))
+    ipv4_firewall_rule = optional(object({
+      name        = string
+      range_end   = string
+      range_start = string
+    }))
+  }))
+}
+
