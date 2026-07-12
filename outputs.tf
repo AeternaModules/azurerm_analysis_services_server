c9c5267cccc8757c3903481f985ed72ec0822dcc
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:21 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,51 +1,46 @@
-output "analysis_services_servers" {
-  description = "All analysis_services_server resources"
-  value       = azurerm_analysis_services_server.analysis_services_servers
-  sensitive   = true
-}
 output "analysis_services_servers_admin_users" {
-  description = "List of admin_users values across all analysis_services_servers"
-  value       = [for k, v in azurerm_analysis_services_server.analysis_services_servers : v.admin_users]
+  description = "Map of admin_users values across all analysis_services_servers, keyed the same as var.analysis_services_servers"
+  value       = { for k, v in azurerm_analysis_services_server.analysis_services_servers : k => v.admin_users }
 }
 output "analysis_services_servers_backup_blob_container_uri" {
-  description = "List of backup_blob_container_uri values across all analysis_services_servers"
-  value       = [for k, v in azurerm_analysis_services_server.analysis_services_servers : v.backup_blob_container_uri]
+  description = "Map of backup_blob_container_uri values across all analysis_services_servers, keyed the same as var.analysis_services_servers"
+  value       = { for k, v in azurerm_analysis_services_server.analysis_services_servers : k => v.backup_blob_container_uri }
   sensitive   = true
 }
 output "analysis_services_servers_ipv4_firewall_rule" {
-  description = "List of ipv4_firewall_rule values across all analysis_services_servers"
-  value       = [for k, v in azurerm_analysis_services_server.analysis_services_servers : v.ipv4_firewall_rule]
+  description = "Map of ipv4_firewall_rule values across all analysis_services_servers, keyed the same as var.analysis_services_servers"
+  value       = { for k, v in azurerm_analysis_services_server.analysis_services_servers : k => v.ipv4_firewall_rule }
 }
 output "analysis_services_servers_location" {
-  description = "List of location values across all analysis_services_servers"
-  value       = [for k, v in azurerm_analysis_services_server.analysis_services_servers : v.location]
+  description = "Map of location values across all analysis_services_servers, keyed the same as var.analysis_services_servers"
+  value       = { for k, v in azurerm_analysis_services_server.analysis_services_servers : k => v.location }
 }
 output "analysis_services_servers_name" {
-  description = "List of name values across all analysis_services_servers"
-  value       = [for k, v in azurerm_analysis_services_server.analysis_services_servers : v.name]
+  description = "Map of name values across all analysis_services_servers, keyed the same as var.analysis_services_servers"
+  value       = { for k, v in azurerm_analysis_services_server.analysis_services_servers : k => v.name }
 }
 output "analysis_services_servers_power_bi_service_enabled" {
-  description = "List of power_bi_service_enabled values across all analysis_services_servers"
-  value       = [for k, v in azurerm_analysis_services_server.analysis_services_servers : v.power_bi_service_enabled]
+  description = "Map of power_bi_service_enabled values across all analysis_services_servers, keyed the same as var.analysis_services_servers"
+  value       = { for k, v in azurerm_analysis_services_server.analysis_services_servers : k => v.power_bi_service_enabled }
 }
 output "analysis_services_servers_querypool_connection_mode" {
-  description = "List of querypool_connection_mode values across all analysis_services_servers"
-  value       = [for k, v in azurerm_analysis_services_server.analysis_services_servers : v.querypool_connection_mode]
+  description = "Map of querypool_connection_mode values across all analysis_services_servers, keyed the same as var.analysis_services_servers"
+  value       = { for k, v in azurerm_analysis_services_server.analysis_services_servers : k => v.querypool_connection_mode }
 }
 output "analysis_services_servers_resource_group_name" {
-  description = "List of resource_group_name values across all analysis_services_servers"
-  value       = [for k, v in azurerm_analysis_services_server.analysis_services_servers : v.resource_group_name]
+  description = "Map of resource_group_name values across all analysis_services_servers, keyed the same as var.analysis_services_servers"
+  value       = { for k, v in azurerm_analysis_services_server.analysis_services_servers : k => v.resource_group_name }
 }
 output "analysis_services_servers_server_full_name" {
-  description = "List of server_full_name values across all analysis_services_servers"
-  value       = [for k, v in azurerm_analysis_services_server.analysis_services_servers : v.server_full_name]
+  description = "Map of server_full_name values across all analysis_services_servers, keyed the same as var.analysis_services_servers"
+  value       = { for k, v in azurerm_analysis_services_server.analysis_services_servers : k => v.server_full_name }
 }
 output "analysis_services_servers_sku" {
-  description = "List of sku values across all analysis_services_servers"
-  value       = [for k, v in azurerm_analysis_services_server.analysis_services_servers : v.sku]
+  description = "Map of sku values across all analysis_services_servers, keyed the same as var.analysis_services_servers"
+  value       = { for k, v in azurerm_analysis_services_server.analysis_services_servers : k => v.sku }
 }
 output "analysis_services_servers_tags" {
-  description = "List of tags values across all analysis_services_servers"
-  value       = [for k, v in azurerm_analysis_services_server.analysis_services_servers : v.tags]
+  description = "Map of tags values across all analysis_services_servers, keyed the same as var.analysis_services_servers"
+  value       = { for k, v in azurerm_analysis_services_server.analysis_services_servers : k => v.tags }
 }
 
